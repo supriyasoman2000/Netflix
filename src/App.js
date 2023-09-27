0d3dfc755f2bc5446a1aa7470c37273899116cc8
supriya <supriyasoman2000@gmail.com>
2023-09-27 14:41:15 +0530

    netflix design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import Banner from './components/Banner';
+import Row from './components/Row';
+import requests from './requests';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>NETFLIX</h1>
+    <div>
+      <Navbar/>
+      <Banner fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row isPoster={true} title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated Movies" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comdey Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+
+
     </div>
+
   );
 }
 
---
+++ src/components/Row.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import tmdbAxiosInstance from '../tmdbAxiosInstance'
+import './Row.css'
+
+function Row({title,fetchUrl,isPoster}) {
+  const [allMovies,setAllMovies] = useState()
+  const base_url = "https://image.tmdb.org/t/p/original/";
+
+
+  const fetchData = async ()=>{
+    const {data} = await tmdbAxiosInstance.get(fetchUrl)
+    setAllMovies(data.results)
+  }
+  //console.log(allMovies);
+  
+   useEffect(()=>{
+    fetchData()
+   },[])
+
+
+  return (
+    <div className='row'>
+      <h1>{title}</h1>
+      <div className='movies-row'>
+        {
+          allMovies?.map(item=>(
+            <img className={`movie ${isPoster && 'movie-poster'}`} src={`${base_url}/${isPoster?item.poster_path:item?.backdrop_path}`} alt="no image" />
+          ))
+        }
+
+      </div>
+    </div>
+  )
+}
+
+export default Row
